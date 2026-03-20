6c6576d0051f7c6c0748ff96c1a39600adbcc251
Fabio Paracchini <fparacchini@alteanet.it>
2026-03-20 12:49:35 +0100

    Fix C++ compilation: add <set> include, replace deprecated ClimateTraits API

--- components/gree/gree.cpp
+++ components/gree/gree.cpp
@@ -106,8 +106,7 @@ climate::ClimateTraits GreeClimate::traits() {
   });
 
   // traits.set_supported_swing_modes(this->supported_swing_modes_);
-  traits.set_supports_current_temperature(true);
-  traits.set_supports_two_point_target_temperature(false);
+  traits.add_feature_flags(climate::CLIMATE_SUPPORTS_CURRENT_TEMPERATURE);
 
   traits.set_supported_presets(this->supported_presets_);
 
